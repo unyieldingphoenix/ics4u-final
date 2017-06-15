d06af220f4a6fd387da51a519c49a6fa429eda29
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-06-14 20:53:19 -0400

    Stylistic changes

--- GameScreen.java
+++ GameScreen.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 import javax.swing.*;
 
-class GameScreen extends JPanel implements ActionListener, Runnable, ComponentListener
+class GameScreen extends JPanel implements ActionListener, ComponentListener, Runnable
 {
 	public static final int FPS = 40; // 20;
 	public static final int delay = 1000 / FPS;
@@ -37,13 +37,13 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 	private static Player mainChar;
 	private static ArrayList<Enemy> enemies;
 	private static Vector2 goalPos;
-	
+
 	private static boolean winLose;
 
 	GameScreen(Dimension dim)
 	{
 		addComponentListener(this);
-		
+
 		blocks = new Block[dim.height / Block.getLen() + edW*2][dim.width / Block.getLen() + edW*2];
 		for(int i = 0; i < blocks.length; ++i)
 			for(int j = 0; j < blocks[i].length; ++j) // Default Tiling
@@ -58,12 +58,12 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 		this.getActionMap().put(JUMP, new SetKeyAction(Entity.UP, true));
 		this.getInputMap(WIFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0, true), JUMP_R);
 		this.getActionMap().put(JUMP_R, new SetKeyAction(Entity.UP, false));
-		
+
 		this.getInputMap(WIFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0, false), DOWN);
 		this.getActionMap().put(DOWN, new SetKeyAction(Entity.DOWN, true));
 		this.getInputMap(WIFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0, true), DOWN_R);
 		this.getActionMap().put(DOWN_R, new SetKeyAction(Entity.DOWN, false));
-		
+
 		this.getInputMap(WIFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0, false), MOVE_LEFT);
 		this.getActionMap().put(MOVE_LEFT, new SetKeyAction(Entity.LEFT, true));
 		this.getInputMap(WIFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0, true), MOVE_LEFT_R);
@@ -73,7 +73,7 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 		this.getActionMap().put(MOVE_RIGHT, new SetKeyAction(Entity.RIGHT, true));
 		this.getInputMap(WIFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0, true), MOVE_RIGHT_R);
 		this.getActionMap().put(MOVE_RIGHT_R, new SetKeyAction(Entity.RIGHT, false));
-		
+
 		this.getInputMap(WIFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_P, 0, false), P_KEY);
 		this.getActionMap().put(P_KEY, new ChangeScreenAction());
 
@@ -96,12 +96,12 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 
 		winLose = false;
 		enemies.clear();
-		
+
 		// Load level
 		StageManager.loadMap(System.getProperty("user.dir") + "/include/levels", fileName, blocks);
 		goalPos = StageManager.getGoalPos(System.getProperty("user.dir") + "/include/levels", fileName, blocks);
 		gameScreen.start();
-		
+
 		// Replace Enemy blocks with Enemies
 		for(int i = 0; i < blocks.length; ++i)
 		{
@@ -116,7 +116,7 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 					mainChar.setPos(new Vector2((j-edW) * Block.getLen(),  (i-edW) * Block.getLen()));
 					mainChar.setVel(new Vector2());
 				}
-				else	// Not an Enemy
+				else	// Not a dynamic Block
 					continue;
 				blocks[i][j].setBlock(Block.AIR);
 			}
@@ -148,7 +148,7 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 			}
 		}
 	}	// end method actionPerformed
-	
+
 	public static Block getBlocks(int y, int x)
 	{
 		return blocks[y+edW][x+edW];
@@ -181,7 +181,7 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 		int bLen = Block.getLen();
 		return (a.X < b.X + bLen && b.X < a.X + bLen && a.Y < b.Y + bLen && b.Y < a.Y + bLen);
 	}	// end method intersects
-	
+
 	public void componentShown(ComponentEvent e)
 	{
 		freeze(false);
@@ -192,13 +192,13 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 	}
 	public void componentResized(ComponentEvent e){   }
 	public void componentMoved(ComponentEvent e){   }
-	
+
 	@Override // Interface: Runnable
 	public void run()
 	{
 		timer.start();
 	}	// end method run
-	
+
 	class ChangeScreenAction extends AbstractAction
 	{		
 		@Override // Superclass: AbstractAction
@@ -207,7 +207,7 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 			CoolPlatformer.changeScreen("PauseScreen");
 		}	// end method ActionPerformed
 	}	// end class ChangeScreenAction
-	
+
 	class SetKeyAction extends AbstractAction
 	{
 		private int indexToSet;
@@ -218,7 +218,7 @@ class GameScreen extends JPanel implements ActionListener, Runnable, ComponentLi
 			this.indexToSet = indexToSet;
 			this.pressedDown = pressedDown;
 		}	// end constructor(int, double)
-		
+
 		@Override // Superclass: AbstractAction
 		public void actionPerformed(ActionEvent ae)
 		{
